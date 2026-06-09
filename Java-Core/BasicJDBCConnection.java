bc85301120b85e96dee57899abf23f4bf47bbdd1
Samyuktha R <21samyuktha@gmail.com>
2026-06-09 16:37:21 +0530

    Create BasicJDBCConnection.java

---
+++ Java-Core/BasicJDBCConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Exercise31_BasicJDBCConnection {
+
+    public static void main(String[] args) {
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/testdb";
+            String user = "root";
+            String password = "root";
+
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Database Connected Successfully");
+
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Connection Failed");
+        }
+    }
+}
