9bed9f9d97180d2d71600cef0338df8da22debff
Samyuktha R <21samyuktha@gmail.com>
2026-06-09 15:59:23 +0530

    Create TypeCastingExample.java

---
+++ Java-Core/TypeCastingExample.java
@@ -0,0 +1,19 @@
+public class Exercise07_TypeCastingExample {
+
+    public static void main(String[] args) {
+
+        double num1 = 45.89;
+
+        int convertedInt = (int) num1;
+
+        System.out.println("Double Value = " + num1);
+        System.out.println("Converted Integer = " + convertedInt);
+
+        int num2 = 25;
+
+        double convertedDouble = num2;
+
+        System.out.println("Integer Value = " + num2);
+        System.out.println("Converted Double = " + convertedDouble);
+    }
+}
